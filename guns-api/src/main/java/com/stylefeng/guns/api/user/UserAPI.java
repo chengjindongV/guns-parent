a661363aa425c6d6120e336f2d0b674c89cf3aa9
chengjindongV <guilinsister@163.com>
2019-09-20 00:04:59 +0800

    dubbo初始化

---
+++ guns-api/src/main/java/com/stylefeng/guns/api/user/UserAPI.java
@@ -0,0 +1,10 @@
+package com.stylefeng.guns.api.user;
+
+/**
+ * @Author: chengjindong
+ * @Date: 2019/9/18 23:29
+ */
+public interface UserAPI {
+
+    boolean login(String username, String password);
+}
